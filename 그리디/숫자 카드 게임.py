47fc1cb49e63ec001e817bb4ac49acc17fdffac8
xanqus <sbk98031816@gmail.com>
2021-08-17 18:18:41 +0900

    숫자 카드 게임

--- 그리디/숫자 카드 게임.py
+++ 그리디/숫자 카드 게임.py
@@ -1,3 +1,15 @@
-C, R = input().split()
-intC = int(C)
-intR = int(R)
+N, M = map(int, input().split())
+
+my_list = [list(map(int, input().split())) for _ in range(N)]
+
+answer_list = list()
+for i in range (N):
+
+    answer_list.append(min(my_list[i]))
+
+print(max(answer_list))
+
+
+
+
+
